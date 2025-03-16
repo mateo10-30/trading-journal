a335189c35b947a35af6d4e1978585088059f1b0
mateo10-30 <mateordasi@gmail.com>
2025-03-16 20:47:15 +0100

    Add files via upload

---
+++ defaultValues.js
@@ -0,0 +1,9 @@
+// Default values for trading journal
+const defaultValues = {
+  "2025-03-14": 60.96,
+  "2025-03-17": 10,
+  // Add more default values as needed
+};
+
+// You can add other constants here as well
+const STARTING_BALANCE = 10000; // 10000 euros starting balance
